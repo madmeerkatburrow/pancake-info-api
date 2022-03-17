22bddb2d2c81b8c73e4c6bdd615a59d71ad62bbd
crocswap123 <crocwap123@gmail.com>
2022-03-18 00:32:23 +0800

    feat: import method from base

--- api/pairs.ts
+++ api/pairs.ts
@@ -26,7 +26,7 @@ let localPairs = {};
 
 export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
   try {
-    if (Object.keys(localPairs).length >= 0 && (Date.now() < lastUpdated + 3 * 60 * 1000 || updating)) {
+    if (Object.keys(localPairs).length > 0 && (Date.now() < lastUpdated + 3 * 60 * 1000 || updating)) {
       return200(res, localPairs);
     }
 
