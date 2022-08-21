4a957ee26bc42cc403f868736f0cc91a9218d368
crocswap123 <crocwap123@gmail.com>
2022-08-21 21:10:53 +0800

    feat: docs

--- utils/constants/blacklist.ts
+++ utils/constants/blacklist.ts
@@ -1,32 +1,32 @@
 // list of lowercase token addresses that are not returned from the API
 export const BLACKLIST = [
-  "0xe40fc6ff5f2895b44268fd2e1a421e07f567e007", // LBC
-  "0xfd158609228b43aa380140b46fff3cdf9ad315de", // LBA
-  "0xc00af6212fcf0e6fd3143e692ccd4191dc308bea", // USD
-  "0x205969b3ad459f7eba0dee07231a6357183d3fb6", // LDB
-  "0x0bd67d358636fd7b0597724aa4f20beedbf3073a", // SOC
-  "0xedf5d2a561e8a3cb5a846fbce24d2ccd88f50075", // SOS
-  "0x702b0789a3d4dade1688a0c8b7d944e5ba80fc30", // XC
-  "0x041929a760d7049edaef0db246fa76ec975e90cc", // TEN
-  "0xba098df8c6409669f5e6ec971ac02cd5982ac108", // BETH
-  "0x1bbed115afe9e8d6e9255f18ef10d43ce6608d94", // BTCST
-  "0xe99512305bf42745fae78003428dcaf662afb35d", // OOE
-  "0xbE609EAcbFca10F6E5504D39E3B113F808389056", // BFI
-  "0x847daf9dfdc22d5c61c4a857ec8733ef5950e82e", // FAN
-  "0xdbf8913dfe14536c0dae5dd06805afb2731f7e7b", // RMFT
-  "0xF23322B390CE57C10c27Ec40Bb3994e47404C99e",
-  "0xadbd1231fb360047525bedf962581f3eee7b49fe", // crona
-  "0xd9f9cbfd6040f2b72153f130c9d4a0a72b859905", // fake test token
-  "0xd9f9CbFd6040f2B72153F130c9d4a0a72b859905",
-  "0xed52eeaeedb59d8623200d18c1ddf1642cb1b915", // cro test
-  "0x08fd6f8d38b00eb024c2c0c3abe56841eed47619", // tesla
-  "0x2d03bece6747adc00e1a131bba1469c15fd11e03",
-  "0x583e9551A570Ec36916dbbEB826cACc6C1B5e65D",
-  "0x50c0C5bda591bc7e89A342A3eD672FB59b3C46a7",
-  "0xf23322b390ce57c10c27ec40bb3994e47404c99e",
-  "0x99002ad0bb0cf2a24cdd1ea6a2ceead775b86923", // merkel
-  "0x99002Ad0bb0cf2A24Cdd1EA6a2ceeaD775b86923",
-  "0xd76dd15ec3f6c48e2aa4cb303c2751f02f82b787", // bl4k
-  "0xD76Dd15EC3f6C48E2aa4cB303C2751f02F82B787",
-  "0xCD6ec1ca885b92d5C7Ee58Dfa6e746c60274AcB1", // cro bargain
+  // "0xe40fc6ff5f2895b44268fd2e1a421e07f567e007", // LBC
+  // "0xfd158609228b43aa380140b46fff3cdf9ad315de", // LBA
+  // "0xc00af6212fcf0e6fd3143e692ccd4191dc308bea", // USD
+  // "0x205969b3ad459f7eba0dee07231a6357183d3fb6", // LDB
+  // "0x0bd67d358636fd7b0597724aa4f20beedbf3073a", // SOC
+  // "0xedf5d2a561e8a3cb5a846fbce24d2ccd88f50075", // SOS
+  // "0x702b0789a3d4dade1688a0c8b7d944e5ba80fc30", // XC
+  // "0x041929a760d7049edaef0db246fa76ec975e90cc", // TEN
+  // "0xba098df8c6409669f5e6ec971ac02cd5982ac108", // BETH
+  // "0x1bbed115afe9e8d6e9255f18ef10d43ce6608d94", // BTCST
+  // "0xe99512305bf42745fae78003428dcaf662afb35d", // OOE
+  // "0xbE609EAcbFca10F6E5504D39E3B113F808389056", // BFI
+  // "0x847daf9dfdc22d5c61c4a857ec8733ef5950e82e", // FAN
+  // "0xdbf8913dfe14536c0dae5dd06805afb2731f7e7b", // RMFT
+  // "0xF23322B390CE57C10c27Ec40Bb3994e47404C99e",
+  // "0xadbd1231fb360047525bedf962581f3eee7b49fe", // crona
+  // "0xd9f9cbfd6040f2b72153f130c9d4a0a72b859905", // fake test token
+  // "0xd9f9CbFd6040f2B72153F130c9d4a0a72b859905",
+  // "0xed52eeaeedb59d8623200d18c1ddf1642cb1b915", // cro test
+  // "0x08fd6f8d38b00eb024c2c0c3abe56841eed47619", // tesla
+  // "0x2d03bece6747adc00e1a131bba1469c15fd11e03",
+  // "0x583e9551A570Ec36916dbbEB826cACc6C1B5e65D",
+  // "0x50c0C5bda591bc7e89A342A3eD672FB59b3C46a7",
+  // "0xf23322b390ce57c10c27ec40bb3994e47404c99e",
+  // "0x99002ad0bb0cf2a24cdd1ea6a2ceead775b86923", // merkel
+  // "0x99002Ad0bb0cf2A24Cdd1EA6a2ceeaD775b86923",
+  // "0xd76dd15ec3f6c48e2aa4cb303c2751f02f82b787", // bl4k
+  // "0xD76Dd15EC3f6C48E2aa4cB303C2751f02F82B787",
+  // "0xCD6ec1ca885b92d5C7Ee58Dfa6e746c60274AcB1", // cro bargain
 ];
